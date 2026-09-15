48099567a929a26acd0edbaa4929305fc63fb497
prokiii18 <k5cvkwgzs6@privaterelay.appleid.com>
2026-09-15 22:40:47 +0200

    Load contact polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './brand-marquee.css';
+import './contact-polish.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
